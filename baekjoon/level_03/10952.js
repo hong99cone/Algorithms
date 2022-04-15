b10870df5d8bcd9caa5cecae76ae184f02cf65d8
HongJu <03mm007mm@gmail.com>
2022-04-15 21:09:29 +0900

    del 10952

--- baekjoon/level_03/10952.js
+++
@@ -1,8 +0,0 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-let length = input.length;
-
-for (let i = 0 ; i < length - 1 ; i++) {
-    let arr = input[i].split(' ').map(Number);
-    answer = arr[0] + arr[1];
-    console.log(answer);
-}
